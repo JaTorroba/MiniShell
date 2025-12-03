e8afefaa6566cb8a78515f69f7e68370d936df19
JaTorroba <j.torroba.2023@alumnos.urjc.es>
2025-12-03 13:41:06 +0100

    Execute commands in children processes. Adjust pipe closure. Free memory in case of error or at the end. Internal commands identification

--- src/myshell.c
+++ src/myshell.c
@@ -3,23 +3,56 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <sys/wait.h>
 
 #include "parser.h"
 
+char *wd;
 
-// char *isInternal(char *name){
+void cd(int argc, char **argv){
+	//TODO check if de directory exists
+	if (argc == 0){
+		//wd = 
+	} else {
+		wd = argv[0];
+		printf("%s",wd);
+	}
+}
 
-// }
+int is_internal(char *name){
+	if (strcmp(name, "cd") == 0 ||
+	 strcmp(name, "jobs") == 0 ||
+	 strcmp(name, "exit") == 0 ||
+	 strcmp(name, "bg") == 0 ||
+	 strcmp(name, "umask") == 0){
+		return 1;
+	} 
+	return 0;
+}
+
+void execute_internal(int argc, char **argv){
+	char *name = argv[0];
+	if (strcmp(name, "cd") == 0) {
+        cd(argc, argv);        
+    } else if (strcmp(name, "jobs") == 0) {
+        // jobs(argv);
+    } else if (strcmp(name, "bg") == 0) {
+        // bg(argv);
+    } else if (strcmp(name, "exit") == 0) {
+        // exit_ms();         
+    } else if (strcmp(name, "umask") == 0) {
+        // umask_ms(argc, argv);
+    }
+}
 
 int main(void) {
 	char buf[1024];
 	tline *line;
-	int i;
+	int i, j, error;
 	pid_t *processes;
 	int **pipes;
 	int fd;
-	int error;
-	//char *internalCommand;
 
 	printf("msh> ");	
 
@@ -28,10 +61,15 @@ int main(void) {
 		error = 0;
 
 		line = tokenize(buf);
-		if (line == NULL) {
+		if (line == NULL) continue;
+
+		//Check if is an internal command
+		if (line->ncommands == 1 && is_internal(line->commands[0].argv[0]) == 1){ 
+			execute_internal(line->commands[0].argc, line->commands[0].argv);
+			printf("msh> ");
 			continue;
 		}
-
+				
 		processes = malloc(sizeof(pid_t) * line->ncommands);
 		pipes = (int **)malloc(sizeof(int *) * (line->ncommands - 1));
 
@@ -40,18 +78,26 @@ int main(void) {
                 pipes[i] = (int *)malloc(sizeof(int) * 2);
 				if (pipe(pipes[i]) < 0){
 					fprintf(stderr, "Error on initializing pipes");
-					error = 1;
+					error = i;
 					break;
 				}
         }
 
-		if (error != 0) continue;
+		if (error != 0){
+			free(processes);
+			for(i = 0; i < error; i++){
+				free(pipes[i]);
+			}
+			free(pipes);
+			printf("msh> ");
+			continue;
+		} 
 
 		for(i = 0; i < line->ncommands; i++){
 			processes[i] = fork();
 			if (processes[i] < 0){
 				fprintf(stderr, "Error on fork\n");
-				error = 2;
+				error = 1;
 				break;
 			}
 
@@ -59,46 +105,68 @@ int main(void) {
 			if (processes[i] == 0) {
 
 				//pipe linking & input, output, error redirections
-				if (i > 0){
-					dup2(pipes[i-1][0], 0); //close stdin & open pipe on reader
-					if (i == line->ncommands - 1){
-						if (line->redirect_output != NULL){
-							fd = open(line->redirect_output, O_WRONLY);
-							if (fd < 0){
-								fprintf(stderr, "Fichero: Error. No se pudo abrir %s\n", line->redirect_output);
-								break;
-							}
-							dup2(fd, 1);
-							close(fd);
-						}
-						if (line->redirect_error != NULL){
-							fd = open(line->redirect_error, O_WRONLY);
-							if (fd < 0){
-								fprintf(stderr, "Fichero: Error. No se pudo abrir %s\n", line->redirect_error);
-								break;
-							}
-							dup2(fd, 2);
-							close(fd);
-						}	
-					}
-				}
-				if (i < line->ncommands - 1){
-					dup2(pipes[i][1], 1); //close stdout & open pipe on writer
-					if (i == 0 && line->redirect_input != NULL){
-						fd = open(line->redirect_input, O_RDONLY);
+				if (i == line->ncommands - 1){
+					if (line->redirect_output != NULL){
+						fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 						if (fd < 0){
-							fprintf(stderr, "Fichero: Error. No se pudo abrir %s\n", line->redirect_input);
-							break;
+							fprintf(stderr, "Fichero: Error. No se pudo abrir %s\n", line->redirect_output);
+							exit(5);
 						}
-						dup2(fd, 0);
+						dup2(fd, 1);
 						close(fd);
 					}
+					if (line->redirect_error != NULL){
+						fd = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+						if (fd < 0){
+							fprintf(stderr, "Fichero: Error. No se pudo abrir %s\n", line->redirect_error);
+							exit(5);
+						}
+						dup2(fd, 2);
+						close(fd);
+					}	
+				}
+				if (i > 0){
+					dup2(pipes[i-1][0], 0); //set stdin to pipe on reader
+				}
+				if (i < line->ncommands - 1){
+					dup2(pipes[i][1], 1); //set stdout to pipe on writer
+				}
+				if (i == 0 && line->redirect_input != NULL){
+					fd = open(line->redirect_input, O_RDONLY);
+					if (fd < 0){
+						fprintf(stderr, "Fichero: Error. No se pudo abrir %s\n", line->redirect_input);
+						exit(5);
+					}
+					dup2(fd, 0);
+					close(fd);
 				}
 
+				for (j = 0; j < line->ncommands - 1; j++){
+					close(pipes[j][0]);
+					close(pipes[j][1]);
+				}
 
-			}
-				
+				execvp(line->commands[i].filename, line->commands[i].argv);
+
+				fprintf(stderr, "Mandato: No se encuentra el mandato");
+				exit(1);
+			} 
 		}
+
+		for(i = 0; i < line->ncommands - 1; i++){
+				close(pipes[i][0]);
+				close(pipes[i][1]);
+				free(pipes[i]);
+		}
+		if (line->ncommands > 1) free(pipes);
+
+
+		for(i = 0; i < line->ncommands; i++){
+			wait(NULL);
+		}
+
+		free(processes);
+
 		printf("msh> ");	
 	}
 			
